d03e2952a3d59ddda5a0b559335ba0ff959fc972
TREND\ext_johnesl <johnes_lee@trend.com.tw>
2018-04-17 11:40:47 +0800

    Add new Employee API

---
+++ APITest/APITest/Controllers/EmployeesController.cs
@@ -0,0 +1,112 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using APITest.Models;
+
+namespace APITest.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: api/Employees
+        public IQueryable<Employees> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET: api/Employees/5
+        [ResponseType(typeof(Employees))]
+        public IHttpActionResult GetEmployees(int id)
+        {
+            Employees employees = db.Employees.Find(id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employees);
+        }
+
+        // PUT: api/Employees/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployees(int id, Employees employees)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employees.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employees).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeesExists(id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Employees
+        [ResponseType(typeof(Employees))]
+        public IHttpActionResult PostEmployees(Employees employees)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employees.Add(employees);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = employees.EmployeeID }, employees);
+        }
+
+        // DELETE: api/Employees/5
+        [ResponseType(typeof(Employees))]
+        public IHttpActionResult DeleteEmployees(int id)
+        {
+            Employees employees = db.Employees.Find(id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employees);
+            db.SaveChanges();
+
+            return Ok(employees);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeesExists(int id)
+        {
+            return db.Employees.Count(e => e.EmployeeID == id) > 0;
+        }
+    }
+}
